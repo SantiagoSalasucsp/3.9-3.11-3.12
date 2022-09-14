bb4b0f0516cb705165c35377170535657297c9da
SantiagoSalasucsp <112534601+SantiagoSalasucsp@users.noreply.github.com>
2022-09-13 22:00:45 -0500

    Update and rename 3.11 to 3.11.h

---
+++ 3.11.h
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class MotorVehicle
+{
+  private:
+    string marca;
+    string combustible;
+    string color;
+    int motor;
+    int tiempo;
+  public:
+    MotorVehicle(string _marca, string _combustible, string _color, int _motor, int _tiempo){
+  _marca=marca;
+  _combustible;
+  _color;
+  _motor;
+  _tiempo;
+    };
+    void setmarca(string _marca)
+      {marca=_marca;}
+      getmarca() const
+      {retutn marca;};
+    void setcombustible(string _combustible)
+      {combustible=_combustible}
+      getcaombustible() const
+      {return combustible;};
+    void setcolor(string _color)
+      {color=_color}
+      getcolor() const
+      {return color;};
+    void setmotor(int _motor)
+      {motor=_motor}
+      getmotor() const
+    void settiempo(int _tiempo)
+      {tiempo=_tiempo}
+      gettiempo() const
